2ae2e6951718090fab59c40f9f20b888ced2dbd2
Kevin <kevin@kvnd.me>
2019-09-07 18:10:57 -0500

    Initlal Layout

--- keyboards/keebio/iris/keymaps/kvnd/config.h
+++ keyboards/keebio/iris/keymaps/kvnd/config.h
@@ -26,3 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#define LSPO_KEYS KC_LSFT, KC_LSFT, KC_LBRACKET
+#define RSPO_KEYS KC_RSFT, KC_RSFT, KC_RBRACKET
