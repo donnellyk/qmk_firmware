d0782677ee4de3ca0904341489e96a99654131a4
Kevin <kevin@kvnd.me>
2021-03-06 18:09:08 -0600

    Proper Lead key.

--- keyboards/crkbd/keymaps/kvnd/config.h
+++ keyboards/crkbd/keymaps/kvnd/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ONESHOT_TAP_TOGGLE 2
 #define TAPPING_TOGGLE 2
 
- #define LEADER_PER_KEY_TIMING
-#define LEADER_TIMEOUT 300
+// #define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 350
 
 #endif
